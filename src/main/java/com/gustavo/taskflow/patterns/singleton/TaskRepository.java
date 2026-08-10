f8b46b0b64f7cbd2dd2be12b83e156816ecd20f6
gustavoh-silva <gustavoh-silva@users.noreply.github.com>
2026-08-10 14:12:41 -0300

    CRUD no taskreporitory

--- src/main/java/com/gustavo/taskflow/patterns/singleton/TaskRepository.java
+++ src/main/java/com/gustavo/taskflow/patterns/singleton/TaskRepository.java
@@ -1,5 +1,12 @@
 package com.gustavo.taskflow.patterns.singleton;
 
+import com.gustavo.taskflow.model.Task;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class TaskRepository {
     private static final TaskRepository instance = new TaskRepository();
 
@@ -9,4 +16,31 @@ public class TaskRepository {
     public static TaskRepository getInstance() {
         return instance;
     }
+
+    private final List<Task> campo = new ArrayList();
+
+    public void addTask(Task task) {
+        this.campo.add(task);
+    }
+
+    public List<Task> getAll(){
+        return this.campo;
+    }
+
+    public Task findById(int id) {
+        for (Task task : this.campo) {
+            if (task.getId() == id) { return task; }
+        }
+        return null;
+    }
+
+    public boolean remove(int id){
+        for (Task task : this.campo){
+            if (task.getId() == id){
+                return this.campo.remove(task);
+            }
+        }
+        return false;
+    }
 }
+
