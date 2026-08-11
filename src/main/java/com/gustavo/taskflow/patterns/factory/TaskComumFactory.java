d2abefc6df2ff82823c7d684519999efc896499b
gustavoh-silva <gustavoh-silva@users.noreply.github.com>
2026-08-11 17:23:30 -0300

    TaskComumFactory devolve TaskComum em vez de null

--- src/main/java/com/gustavo/taskflow/patterns/factory/TaskComumFactory.java
+++ src/main/java/com/gustavo/taskflow/patterns/factory/TaskComumFactory.java
@@ -2,10 +2,11 @@ package com.gustavo.taskflow.patterns.factory;
 
 import com.gustavo.taskflow.model.Priority;
 import com.gustavo.taskflow.model.Task;
+import com.gustavo.taskflow.model.TaskComum;
 
 public class TaskComumFactory implements TaskFactory{
     @Override
     public Task createTask(String titulo, long id, Priority prioridade) {
-        return null;
+        return new TaskComum(titulo, id, prioridade);
     }
 }
